1293e41d181920177d566524487e0c869e928e9b
levinem <14362448+levinem@users.noreply.github.com>
2021-03-04 23:50:21 -0500

    Started building out some of the main classes.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <ifstream>
+#include <vector>
+using namespace std;
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+struct Job {
+
+};
+
+class Client {
+private:
+
+public:
+    Client();
+    ~Client();
+
+};

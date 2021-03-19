023518c87f5cb8eff3079c72540a464408db9b3b
levinem <14362448+levinem@users.noreply.github.com>
2021-03-18 23:36:29 -0400

    Updated the Job class to include some private fields useful for calculating a job's progress. Filled out some function definitions in the class.

--- Job.cpp
+++ Job.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "Job.h"
+
+Job::Job(SharedFiles files, ofstream code) {
+    ofstream = code;
+    shared_files = files;
+    percent_completed = 0.0;
+    current_runtime = 0.0;
+    max_runtime = 50000000.0;
+}
+
+Job::~Job() {
+    ofstream = nullptr;
+    ~shared_files;
+}
+
+void Job::UpdateRuntime(double completed_sub_job_runtime) {
+    current_runtime += completed_sub_job_runtime;
+}
+
+void Job::UpdatePercentCompleted() {
+    percent_completed = sub_jobs_left_to_run / total_number_of_sub_jobs;
+}
--- Job.h
+++ Job.h
@@ -4,55 +4,71 @@
 
 #ifndef HTSTORAGE_JOB_H
 #define HTSTORAGE_JOB_H
+#include <fstream>
 #include "SharedFiles.h"
 
 class Job {
-    public:
+private:
+    ofstream code;
+    SharedFiles shared_files;
+    int sub_jobs_left_to_run;
+    int total_number_of_sub_jobs;
+    double percent_completed;
+    double current_runtime;
+    double max_runtime;
+public:
     //client
 
-        /**
-         * create a job object from the client
-         * @param files associated files object
-         * @param code code to be run (may not be best as a char* unsure what to do)
-         */
-        Job(SharedFiles files, char * code);
+    /**
+     * create a job object from the client
+     * @param files associated files object
+     * @param code code to be run (may not be best as a char* unsure what to do)
+     */
+    Job(SharedFiles files, ofstream code);
+    ~Job();
 
-        /**
-         * set up a way for job to be requested by storage
-         */
-        void Allow_pull_job();
+    // Update the current runtime of the job.
+    void UpdateRuntime(double completed_partial_job_runtime);
+
+    void UpdatePercentCompleted();
+
+    void UpdateNumberOfRemainingJobs();
+    /**
+     * set up a way for job to be requested by storage
+     */
+    void AllowPullJob();
     //manager
 
-        /**
-         * get the estimated time of the job
-         * @return the estimated time in ms of the job
-         */
-        int Get_time();
+    /**
+     * get the estimated time of the job
+     * @return the estimated time in ms of the job
+     */
+    int GetTime();
     //execution
-        /**
-         * run the job on the server
-         */
-        void Run();
+    /**
+     * run the job on the server
+     */
+    void Run();
 
-        /**
-         * get this job's info from client
-         */
-        void Pull_job();
+    /**
+     * get this job's info from client
+     */
+    void PullJob();
 
-        /**
-         * get the attached Shared_files object
-         */
-        SharedFiles get_storage();
+    /**
+     * get the attached Shared_files object
+     */
+    SharedFiles GetStorage();
 
     //network
-        /**
-         * create a serialized object for sending over the network
-         */
-        char* SerializeSelf();
-        /**
-         * create a files object to add files to from a serialized object that was sent over the network
-         */
-        Job(char* serializedVersion);
+    /**
+     * create a serialized object for sending over the network
+     */
+    char* SerializeSelf();
+    /**
+     * create a files object to add files to from a serialized object that was sent over the network
+     */
+    Job(char* serializedVersion);
 
 
 };

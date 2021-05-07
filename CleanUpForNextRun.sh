4d0130f1a55a7dea3edc2263f4a0c1e2df0dc2f3
levinem <14362448+levinem@users.noreply.github.com>
2021-05-06 22:57:49 -0400

    Updated the scripts for cleaning up and rerunning the code.

---
+++ CleanUpForNextRun.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Clean up the client workspaces and files.
+mkdir temp
+cp tests/files/testfile3.txt tests/files/testfile4.txt temp
+sudo rm -r -f tests/files/*
+sudo mv temp/* tests/files/
+sudo rm -r -f temp
+
+# Rebuild the client workspaces and files.
+cd tests/files/
+mkdir {0..10}
+for dir in */; do
+    cp testfile3.txt testfile4.txt $dir
+done
+cd ../..
+
+# Clean up the working directory.
+rm -r -f workingDir/execution/* workingDir/storage1/* workingDir/storage2/*
--- SetUpAWSInstance.sh
+++ SetUpAWSInstance.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
+# Install the dependencies.
 sudo snap install emacs --classic
 sudo snap install cmake --classic
 sudo apt install g++ make -y
+
+# Build the working directory.
+mkdir workingDir
+cd workingDir
+mkdir execution storage1 storage2
+cd ..
+
+# Build the client workspaces and files.
+cd tests/files/
+mkdir {0..10}
+for dir in */; do
+    cp testfile3.txt testfile4.txt $dir
+done
+cd ../..
+
+# Initiate the build.
 mkdir build
 cd build
 sudo cmake ..
 sudo make
+
+# Make the other bash scripts executable.
+chmod +x Rebuild.sh
+chmod +x CleanUpForNextRun.sh
